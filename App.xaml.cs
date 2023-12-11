a661adc2929eab8d9a67d7e7e37230a3e0e27900
RODICA BOTA <rodica.bota@stud.ubbcluj.ro>
2023-12-11 20:09:47 +0200

    LAB-8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace BOTA_RODICA_LAB7
+﻿using System;
+using BOTA_RODICA_LAB7;
+using System.IO;
+using BOTA_RODICA_LAB7.Data;
+
+namespace BOTA_RODICA_LAB7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        { 
+            get 
+            { 
+                if (database == null) 
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                } 
+                return database; 
+            }
+        }
         public App()
         {
             InitializeComponent();
